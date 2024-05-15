ef1849852d728e07d0ffc7fdffc638f3309ef625
DavonnEscobilla <qdpescobilla@tip.edu.ph>
2024-05-15 21:32:43 +0800

    Updated Frontend

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,9 +12,9 @@ export default function FooterCom() {
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                Group One
+                PsyKitz
               </span>
-              Blog
+              Blogs
             </Link>
           </div>
           <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
@@ -33,7 +33,7 @@ export default function FooterCom() {
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Group One Blog
+                  PsyKitz Blogs
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -41,7 +41,7 @@ export default function FooterCom() {
               <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.github.com/sahandghavidel'
+                  href='https://github.com/DavonnEscobilla/Sofdes-Finals'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,22 +4,21 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Flow Charts!
+            About PsyKitz Blogs!
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Group One!
+              Welcome to PsyKitz!
             </p>
 
             <p>
-              On this blog, you'll find weekly articles and tutorials on topics
-              such as web development, software engineering, and programming
-              languages!
+              On this platform, you'll find weekly articles and tutorials on topics
+              about programming using flow charts!
             </p>
 
             <p>
               We encourage you to leave comments on our posts and engage with
-              other readers. You can like other people's comments and reply to
+              other people. You can like other people's comments and reply to
               them as well. We believe that a community of learners can help
               each other grow and improve.
             </p>
